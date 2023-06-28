c71c9a7ad93ee3d84896f3163d3fd55db52f2510
mint <ge10rik@gmail.com>
2023-06-28 23:31:07 +0700

    header navigation

---
+++ src/components/TheHamburger.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import { ReactComponent as HamburgerIcon } from '../assets/shared/icon-hamburger.svg';
+import { ReactComponent as CloseIcon } from '../assets/shared/icon-close.svg';
+
+interface TheHamburgerProps {
+  isToggled: boolean;
+  onToggle: () => void;
+  className?: string;
+}
+
+const TheHamburger: FC<TheHamburgerProps> = ({ isToggled, className = '', onToggle }) => {
+  return (
+    <button className={`absolute z-50 md:hidden ${className}`} onClick={onToggle}>
+      {isToggled ? <CloseIcon /> : <HamburgerIcon />}
+    </button>
+  );
+};
+
+export default TheHamburger;
--- src/components/TheHeader.tsx
+++ src/components/TheHeader.tsx
@@ -1,5 +1,7 @@
-import { ReactComponent as Logo } from '../assets/shared/logo.svg';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as Logo } from '../assets/shared/logo.svg';
+import TheHamburger from './TheHamburger';
 
 interface GetClassName {
   (props: { isActive: boolean; isPending: boolean }): string | undefined;
@@ -13,26 +15,56 @@ const TheHeader = () => {
     { path: '/technology', title: 'technology' },
   ];
 
+  const [isHamburgerToggled, setIsHamburgerToggled] = useState(false);
+
   const getClassName: GetClassName = ({ isActive }) =>
-    `uppercase py-8 font-barlow-cond tracking-[2.7px] border-b-[3px] border-b-white/0 ${
-      isActive ? 'border-b-white' : 'hover:border-b-white/[.5]'
+    `uppercase py-2 md:py-8 font-barlow-cond tracking-[2.7px] border-b-[3px] border-b-white/0 ${
+      isActive ? 'border-0 md:border-b-white' : 'hover:border-b-white/[.5]'
     }`;
 
+  const handeHambburgerToggle = () => {
+    setIsHamburgerToggled(!isHamburgerToggled);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      setIsHamburgerToggled(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isHamburgerToggled) {
+      window.addEventListener('resize', handleResize);
+    }
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isHamburgerToggled]);
+
   const content = navItems.map(({ path, title }, idx) => (
-    <li className="cursor-pointer py-8" key={path}>
-      <NavLink to={path} className={getClassName}>
-        <span className="mr-2 font-bold">{`0${idx}`}</span>
+    <li className="cursor-pointer py-2 md:py-8 md:text-sm xl:text-base" key={path}>
+      <NavLink to={path} className={getClassName} onClick={() => setIsHamburgerToggled(false)}>
+        <span className="mr-2 font-bold md:hidden xl:inline">{`0${idx}`}</span>
         {title}
       </NavLink>
     </li>
   ));
+
   return (
-    <header className="flex items-center justify-between">
+    <header className="relative flex items-center justify-between xl:my-10 xl:after:relative xl:after:order-1 xl:after:h-[1px] xl:after:w-full xl:after:bg-white/25 xl:after:content-['']">
       <div>
         <Logo className="mx-[clamp(1.5rem,5vw,3.5rem)] my-6" />
       </div>
-      <nav>
-        <ul className="flex gap-[clamp(1.5rem,5vw,3.5rem)] bg-white/[0.05] backdrop-blur-2xl md:px-[clamp(3rem,7vw,7rem)]">
+      <TheHamburger
+        className="md:d right-6 top-8"
+        isToggled={isHamburgerToggled}
+        onToggle={handeHambburgerToggle}
+      />
+      <nav className="xl:order-2">
+        <ul
+          className={`${
+            isHamburgerToggled ? 'translate-x-[0]' : ''
+          } fixed inset-y-0 left-1/3 right-0 flex translate-x-full flex-col gap-[clamp(1.5rem,5vw,3.5rem)] bg-white/[0.05] px-8 py-[min(20rem,15vh)] backdrop-blur-2xl transition duration-500 ease-in-out md:static md:transform-none md:flex-row md:px-[clamp(3rem,7vw,7rem)] md:py-0`}
+        >
           {content}
         </ul>
       </nav>
